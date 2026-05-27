da5a43a5837a92b64f6222ce7baaef6d505366dd
Damilare Osunrinde <89488280+Damilare-prog@users.noreply.github.com>
2026-05-27 09:59:46 +0100

    Update Next.js configuration for base path and asset prefix

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  distDir: 'out',
+  basePath: '/inside-wave-chronicles',
+  assetPrefix: '/inside-wave-chronicles/',
   images: {
     unoptimized: true,
   },
+  trailingSlash: true,
 }
 
 module.exports = nextConfig
